e52f706eaabfcdb7feca74b2eaf35c985e55a921
Fran Linde Blazquez <franlindebl@gmail.com>
2023-03-11 19:05:52 +0100

    Added samples for testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React from 'react';
+import Note from './components/Note/Note';
+import { SAMPLE_NOTES } from './sample-notes';
+import Togglable from './components/Togglable/Togglable';
 
 function App() {
+  const [notes, setNotes] = React.useState(SAMPLE_NOTES);
+
+  const toggleImportanceOf = (noteId) => {
+    const newNotes = notes.map((note) => {
+      if (note.id === noteId) {
+        return {
+          ...note,
+          important: !note.important,
+        };
+      }
+      return note;
+    });
+
+    setNotes(newNotes);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Prueba de testing</h1>
+
+      <h2>Notes:</h2>
+      {notes.map((note) => (
+        <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
+      ))}
+
+      <h2>Togglable:</h2>
+      <Togglable buttonLabel='Oculto'>
+        <p>Este es un contenido oculto!</p>
+      </Togglable>
+
+      <Togglable buttonLabel='También oculto'>
+        <p>Hola amigos</p>
+        <p>Esto también es un contenido oculto...</p>
+      </Togglable>
     </div>
   );
 }
---
+++ src/sample-notes.js
@@ -0,0 +1,52 @@
+export const SAMPLE_NOTES = [
+  {
+    id: 1,
+    content: 'Poner una lavadora',
+    important: false,
+  },
+  {
+    id: 2,
+    content: 'Comprar leche',
+    important: true,
+  },
+  {
+    id: 3,
+    content: 'Llamar al dentista para hacer una cita',
+    important: true,
+  },
+  {
+    id: 4,
+    content: 'Sacar la basura',
+    important: false,
+  },
+  {
+    id: 5,
+    content: 'Revisar el correo',
+    important: false,
+  },
+  {
+    id: 6,
+    content: 'Comprar regalos de cumpleaños para la familia',
+    important: true,
+  },
+  {
+    id: 7,
+    content: 'Hacer la cama',
+    important: false,
+  },
+  {
+    id: 8,
+    content: 'Limpiar la cocina',
+    important: true,
+  },
+  {
+    id: 9,
+    content: 'Hacer ejercicio',
+    important: false,
+  },
+  {
+    id: 10,
+    content: 'Leer un libro',
+    important: false,
+  },
+];
