db4684138c9c130001886aa637340a73308cbf04
Fran Linde Blazquez <franlindebl@gmail.com>
2023-03-11 20:28:02 +0100

    First test on App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText('Prueba de testing');
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('search the page title (h1)', () => {
+    render(<App />);
+    const title = screen.getByText('Prueba de testing');
+    expect(title).toBeInTheDocument();
+  });
+
+  test('search notes title', () => {
+    render(<App />);
+    const title = screen.getByText('Notes:');
+    expect(title).toBeInTheDocument();
+  });
+
+  // test can be called IT
+  it('search the togglable title', () => {
+    render(<App />);
+    const title = screen.getByText('Togglable:');
+    expect(title).toBeInTheDocument();
+  });
 });
